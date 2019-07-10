c533635bde49dc79cd226a84c2f652bba701aae7
Kelly Salamack <kellysalamackdesign@gmail.com>
2019-07-10 09:55:15 -0400

    concept logo and favicon - added card shadow

--- public/assets/js/myshelf.js
+++ public/assets/js/myshelf.js
@@ -26,7 +26,7 @@ $(function () {
             result.forEach(element => {
                 $("#shelfdisplay").append(`
                 <div class="d-inline-block card" style="width: 18rem;">
-                <img src=${element.imageURL} class="card-img-top img-fluid" alt="...">
+                <img src=${element.imageURL} class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${element.item_name}</h5>
                     <p class="card-text">Category: ${element.category}</p>

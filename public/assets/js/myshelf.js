700756818c87df36a7d13be101e915fc36678702
PaulinaBootcamp <paulinaoshafi@gmail.com>
2019-07-12 22:58:11 -0400

    Fixed add modal

--- public/assets/js/myshelf.js
+++ public/assets/js/myshelf.js
@@ -1,12 +1,5 @@
 $(function () {
 
-    var d = new Date();
-    var strDate = d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate();
-    console.log(strDate);
-
-    var dt = new Date(d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate());
-    dt.setMonth(dt.getMonth() + 4);
-    console.log(dt.toLocaleDateString());
 
 
     $.get("/api/myshelf", function (result) {
@@ -55,40 +48,60 @@ $(function () {
         e.preventDefault();
 
         $.post("/api/myshelf/additem", function (result) {
-            console.log("Added new item.");
         });
     });
 
 
+
     //add item by UPC code
+
     // This .on("click") function will trigger the AJAX Call
     $("#searchByUPC").on("click", function (event) {
         // event.preventDefault() can be used to prevent an event's default behavior.
         // Here, it prevents the submit button from trying to submit a form when clicked
         event.preventDefault();
 
-        // Here we grab the text from the input box
-        var upc = $("#item_UPC").val().trim();
 
-        // Here we construct our URL
-        var queryURL = "http://localhost:8080/lookup/" + upc
+        $("#item_UPC_warning_msg").empty();
+
+        if ($("#item_UPC").val().trim().length === 12 || !isNaN($("#item_UPC")) || ($("#item_UPC")) === null) {
+            // Here we grab the text from the input box
+            var upc = $("#item_UPC").val().trim();
+
+            // Here we construct our URL
+            var queryURL = "http://localhost:8080/lookup/" + upc
+
+            //Empty warning message
+            $("#item_UPC_warning_msg").append();
+
+            $.ajax({
+                url: queryURL,
+                method: "GET",
+            }).then(function (response) {
+                console.log(response.code);
+                if (response.code === "OK" && response.items.length > 0) {
+                    $("#item_name").val(response.items[0].title).val();
+                    $("#price").val(response.items[0].offers[0].price).val();
+                    $("#imageURL").val(response.items[0].images[0]).val()
+                }
+                else {
+                    $("#item_UPC_warning_msg").append("Item not found")
+                }
+            });
 
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            $("#item_name").val(response.items[0].title).val();
-            $("#price").val(response.items[0].offers[0].price).val();
-            $("#imageURL").val(response.items[0].images[0]).val();
-        });
+        } else {
 
-    })
+            $("#item_UPC_warning_msg").append("Please enter a 12 digit UPC code to search")
+        }
+    });
 
     $("#addToShelf").on("click", function (event) {
         event.preventDefault();
 
+        $("#item_name_warning_msg").empty();
 
+        //Here we will determine if tax rate needs to be applied to the product
         if ($("#add_tax").is(':checked')) {
             //how to capture logged in user tax rate?
             var tax_rate = 0.0625;
@@ -96,30 +109,63 @@ $(function () {
             var tax_rate = 0;
         }
 
-        var shelf_life = Number($("#shelf_life").val().trim());
+        //Here we calculate the expiry date based on today's date and user provided shelf life 
+        shelf_life = Number($("#shelf_life").val().trim());
+        if (shelf_life > !0) {
+            shelf_life = 0
+        }
+
         var d = new Date();
         var dt = new Date(d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate());
         dt.setMonth(dt.getMonth() + shelf_life);
 
+        if ($("#item_name").val().trim() === "" || ($("#item_name")) === null) {
+            $("#item_name_warning_msg").append("Item name is required")
+        } else {
+            // Here we pass undefined to the database if the shelf_life form field is blank
+            if ($("#shelf_life").val().trim() === "") {
+                var shelf_life = undefined
+            } else {
+                var shelf_life = $("#shelf_life").val().trim()
+            };
 
-        var newItem = {
-            item_name: $("#item_name").val().trim(),
-            item_UPC: $("#item_UPC").val().trim(),
-            imageURL: $("#imageURL").val().trim(),
-            shelf_life: $("#shelf_life").val().trim(),
-            category: $("#category").val().trim(),
-            price: $("#price").val().trim(),
-            status: $("#status").val().trim(),
-            label: $("#tag").val().trim(),
-            tax: $("#price").val().trim() * tax_rate,
-            expiry_date: dt.toLocaleDateString()
+            // Here we pass undefined to the database if the price form field is blank
+            if ($("#price").val().trim() === "") {
+                var price = undefined
+            } else {
+                var price = $("#shelf_life").val().trim()
+            };
 
+            // Here we pass placeholder image to the database if the imageURL form field is blank
+            if ($("#imageURL").val().trim() === "") {
+
+                //CAN WE COME UP WITH A FUN PLACEHOLDER IMAGE?
+                var imageURL = "http://www.stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder-350x350.png"
+            } else {
+                var imageURL = $("#imageURL").val().trim()
+            };
+
+            //Add if statement for if empty pass undefinded
+            var newItem = {
+                item_name: $("#item_name").val().trim(),
+                item_UPC: $("#item_UPC").val().trim(),
+                imageURL: imageURL,
+                shelf_life: shelf_life,
+                category: $("#category").val().trim(),
+                price: price,
+                status: $("#status").val().trim(),
+                label: $("#tag").val().trim(),
+                tax: $("#price").val().trim() * tax_rate,
+                expiry_date: dt.toLocaleDateString()
+
+            };
+
+            console.log(newItem);
+
+            addItem(newItem);
+            clear();
+            $("#addByUPC").modal('hide')
         };
-
-        console.log(newItem);
-
-        addItem(newItem);
-        clear();
     });
 
     // Add new item to the database
@@ -137,8 +183,6 @@ $(function () {
         $("#item_name").val("");
         $("#imageURL").val("");
         $("#shelf_life").val("");
-        // $("#category").val("");
-        // $("#status").val("");
         $("#price").val("");
         $("#item_UPC").val("")
         $("#tag").val("")

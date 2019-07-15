887dd72b5d6a9b81f10b8759e6c74a5900bc3791
Blue Tortoise <dianmak@icloud.com>
2019-07-15 18:50:28 -0400

    fjdklsa

--- controllers/shelfController.js
+++ controllers/shelfController.js
@@ -245,7 +245,8 @@ router.get("/", function (req, res) {
     if (req.session.userID === undefined) {
         return res.redirect("/login");
     }
-    res.sendFile("/views/index.html");
+    // res.sendFile("/views/index.html");
+    res.sendFile(path.join(__dirname, "/views/login.html"));
 });
 
 router.get("/wallet", function (req, res) {
